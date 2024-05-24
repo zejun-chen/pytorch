32fa39ea321a128393bcc4817e026690dcdb688a
Chen, Zejun <zejun.chen@intel.com>
2024-05-24 17:03:56 +0800

    remove unregister method
    Signed-off-by: Chen, Zejun <zejun.chen@intel.com>

--- c10/core/KinetoPluginAPI.cpp
+++ c10/core/KinetoPluginAPI.cpp
@@ -9,42 +9,25 @@
 namespace c10 {
 namespace kineto_plugin {
 
-static std::map<DeviceType, std::vector<PluginFactory>> plugin_factories;
+static std::map<DeviceType, PluginFactory> plugin_factories;
 static std::mutex mutex;
 
 void insertKinetoPluginRegister(DeviceType t, PluginFactory f) {
   std::scoped_lock<std::mutex> lock(mutex);
   auto it = plugin_factories.find(t);
   if (it != plugin_factories.end()) {
-    it->second.push_back(f);
+    it->second = f;
   } else {
-    plugin_factories[t] = std::vector<PluginFactory>{f};
+    plugin_factories[t] = f;
   }
-}
-
-void removeKinetoPluginRegister(DeviceType t) {
-  std::scoped_lock<std::mutex> lock(mutex);
-  auto it = plugin_factories.find(t);
-  if (it != plugin_factories.end())
-    plugin_factories.erase(it);
 }
 
 void registerKinetoPluginProfiler() {
   for (const auto& pair : plugin_factories) {
-    const auto& factories = pair.second;
-    for (const auto& factory : factories)
-      libkineto::api().registerProfilerFactory(factory);
-  }
+    const auto& factory = pair.second;
+    libkineto::api().registerProfilerFactory(factory);
 }
 
-void registerKinetoPluginProfiler(DeviceType t) {
-  auto it = plugin_factories.find(t);
-  if (it != plugin_factories.end()) {
-    const auto& factories = it->second;
-    for (const auto& factory : factories)
-      libkineto::api().registerProfilerFactory(factory);
-  }
-}
 
 } // namespace kineto_plugin
 } // namespace c10
--- c10/core/KinetoPluginAPI.h
+++ c10/core/KinetoPluginAPI.h
@@ -12,9 +12,7 @@ namespace kineto_plugin {
 
 using PluginFactory = libkineto::ChildActivityProfilerFactory;
 void C10_API insertKinetoPluginRegister(DeviceType t, PluginFactory f);
-void C10_API removeKinetoPluginRegister(DeviceType t);
 void C10_API registerKinetoPluginProfiler();
-void C10_API registerKinetoPluginProfiler(DeviceType t);
 
 struct KinetoPluginRegister {
   explicit KinetoPluginRegister(DeviceType t, PluginFactory f) {
@@ -25,17 +23,6 @@ struct KinetoPluginRegister {
 #define REGISTER_KINETO_PLUGIN(t, f)                        \
   namespace {                                               \
   static KinetoPluginRegister kineto_plugin_register(t, f); \
-  }
-
-struct KinetoPluginRemover {
-  explicit KinetoPluginRemover(DeviceType t) {
-    c10::kineto_plugin::removeKinetoPluginRegister(t);
-  }
-};
-
-#define UNREGISTER_KINETO_PLUGIN(t)                    \
-  namespace {                                          \
-  static KinetoPluginRemover kineto_plugin_remover(t); \
   }
 
 } // namespace kineto_plugin
